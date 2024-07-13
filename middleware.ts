86ac6e16e91d4452bc8f51895a21098c7fbe6945
William Chow <williamchow604@gmail.com>
2024-07-12 17:47:18 -0700

    Changed matcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)', '/']
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
 
 export async function middleware(req:any) {
