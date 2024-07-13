4bfd9368a1d371d1b22484b79c50f0b6c68dcc1e
William Chow <williamchow604@gmail.com>
2024-07-12 17:16:23 -0700

    Swapped JWT_SECRET with NEXTAUTH_SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -34,7 +34,7 @@ export default NextAuth({
             authorization: LOGIN_URL
         })
     ],
-    secret: process.env.JWT_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: '/login'
     },
